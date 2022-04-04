b81801ae773be6d2bbcd503c13f89f8bd0b5dbbf
Roqaia Alrfou--personal account <85272476+RoqaiaRF@users.noreply.github.com>
2022-04-04 15:30:22 +0300

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,7 +1,69 @@
-exports.handler = function(context, event, callback) {
-    const result = {
-        message : "Hello World!"
-    }
+const axios = require('axios');
+const qs = require('querystring');
 
-    callback(null, result);
+exports.handler = function(context, event, callback) {
+
+  let twiml = new Twilio.twiml.MessagingResponse();
+
+  let { 
+      ApiVersion,
+      SmsSid,
+      SmsStatus,
+      SmsMessageSid,
+      NumSegments,
+      ToState,
+      From,
+      MessageSid,
+      AccountSid,
+      ToCity,
+      FromCountry,
+      ToZip,
+      FromCity,
+      To,
+      FromZip,
+      ToCountry,
+      Body,
+      NumMedia,
+      FromState 
+  } = event;
+
+  let requestBody = {
+      ApiVersion,
+      SmsSid,
+      SmsStatus,
+      SmsMessageSid,
+      NumSegments,
+      ToState,
+      From,
+      MessageSid,
+      AccountSid,
+      ToCity,
+      FromCountry,
+      ToZip,
+      FromCity,
+      To,
+      FromZip,
+      ToCountry,
+      Body,
+      NumMedia,
+      FromState 
   };
+
+    let url1 = "https://bot.snapbot.app/demo";
+    let url2 = "https://app.respond.io/twilio/whatsapp/webhook/";
+
+   const config = {
+     headers: {
+     'Content-Type': 'application/x-www-form-urlencoded'
+     }};
+
+  Promise.all([
+       axios.post(url1, qs.stringify(requestBody), config),
+       axios.post(url2, qs.stringify(requestBody), config)
+       ]).then(result => {
+           callback(null, twiml);
+       }).catch(err => {
+           console.log(err);
+           callback(err);
+       });
+};
